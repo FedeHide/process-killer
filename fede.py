999379c74c7ff85d787eacf6093a633e0b63f092
FedeHide <larcdian@gmail.com>
2024-11-14 20:22:38 -0300

    feat: add luly's configuration

---
+++ fede.py
@@ -0,0 +1,24 @@
+processes_to_kill = [
+    ## fede
+    # anti-cheats
+    "PnkBstrA.exe",
+    "vgtray.exe",
+    # adobe
+    '"Adobe Crash Processor.exe"',
+    '"Adobe Desktop Service.exe"',
+    "AdobeIPCBroker.exe",
+    "AdobeNotificationClient.exe",
+    "AdobeNotificationHelper.exe",
+    "AdobeUpdateService.exe",
+    "CCXProcess.exe",
+    "CoreSync.exe",
+    '"Creative Cloud.exe"',
+    '"Creative Cloud Helper.exe"',
+    '"Creative Cloud UI Helper.exe"',
+    # others
+    "PowerToys.exe",
+    "gamingservices.exe",
+    "gamingservices.exe",
+    "GamingServicesNet.exe",
+    "gamingservicesnet.exe",
+]
--- luly.py
+++ luly.py
@@ -1,6 +1,3 @@
-import subprocess
-
-# List of processes to kill
 processes_to_kill = [
     ## Luly
     # adobe
@@ -32,24 +29,3 @@ processes_to_kill = [
     "gamingservicesnet.exe",
     "GamingServicesNet.exe",
 ]
-
-
-# Function to kill a process by name
-def kill_process(process_name):
-    try:
-        subprocess.run(f"taskkill /IM {process_name} /F", shell=True, check=True)
-    except subprocess.CalledProcessError:
-        pass  # Silently handle the error and continue
-
-
-# Run the script to kill all processes in the list
-def kill_all_processes():
-    for process in processes_to_kill:
-        kill_process(process)
-
-
-# Start execution
-kill_all_processes()
-
-# Wait for user to press any key before exiting
-input("Press any key to exit...")
